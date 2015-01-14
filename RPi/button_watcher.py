fb78f9dbfa4145bb55cda143d97c95940cd34e60
Conrad H. Appel, IV <conrad.h.appel.iv@gmail.com>
2015-01-14 12:32:12 -0600

    button_watcher should now restart running if already running
    needs testing

--- RPi/button_watcher.py
+++ RPi/button_watcher.py
@@ -8,6 +8,7 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+run_process = subprocess.Popen("exec", shell=True) # "empty" constructor? Probably a better way to do this
 
 # main loop
 while True:
@@ -15,5 +16,7 @@ while True:
         subprocess.call("/home/pi/SMU-KNW2300/RPi/update.sh", shell=True)
         time.sleep(2)
     if(GPIO.input(26) == False):
-        subprocess.call("/home/pi/SMU-KNW2300/RPi/run.sh", shell=True)
+	if run_process.poll() is not None:
+		run_process.terminate()
+        run_process = subprocess.Popen("exec /home/pi/SMU-KNW2300/RPi/run.sh", shell=True) # exec so run.sh inherits the shell process
         time.sleep(2)
